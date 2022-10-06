5d4bd9fb8026e6e4521dbdf755bb0672f71e44ae
danbaziewicz <danxbazi@gmail.com>
2022-10-06 18:51:46 -0300

    criação de app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from 'express'
+//import User from "./database/User.js"
+import userController from './controller/user-controller.js'
 
 const app = express()
 
 app.use(express.json())
+
+userController(app)
 
 export default app
